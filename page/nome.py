6b7fcf8d70c7f18c0d634fe7f931dba35c429a5f
Dennis Orion <156465420+xorionx-lab@users.noreply.github.com>
2024-05-11 21:34:23 +0000

    atual

--- page/nome.py
+++ page/nome.py
@@ -1,3 +1,12 @@
 import streamlit as st
+import pandas as pd
 
 tab1, tab2, tab3 = st.tabs(["pagina 1", "pagina 2", "pagine 3"])
+
+tab1.write("""## Primeira pagina""")
+
+df = pd.read.csv("")
+tab2.line_char(df, x="linguagem", y= "desenvolvedores")
+
+with tab3:
+    
